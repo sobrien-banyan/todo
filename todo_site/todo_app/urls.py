12d0dca3399a24d9224e2080e30a7216c8713282
sobrien-banyan <sobrien@banyanlabs.io>
2022-11-28 18:10:53 -0700

    update and delete

--- todo_site/todo_app/urls.py
+++ todo_site/todo_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from todo_app.views import HomeView
+from todo_app.views import HomeView, TaskDetailsView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('<int:task_id>', TaskDetailsView.as_view(), name='task_details'),
 ]
--- todo_site/todo_app/views.py
+++ todo_site/todo_app/views.py
@@ -23,9 +23,35 @@ class HomeView(View):
         )
 
     def post(self, request):
-        print(request.POST)
         task_form = TaskForm(request.POST)
-
         task_form.save()
 
         return redirect('home')
+
+class TaskDetailsView(View):
+    def get(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+
+        task_form = TaskForm(instance=task)
+        
+        html_data = {
+            'task_object': task,
+            'form': task_form,
+        }
+
+        return render(
+            request=request,
+            template_name='detail.html',
+            context=html_data,
+        )
+
+    def post(self, request, task_id):
+         task = Task.objects.get(id=task_id)
+
+         if 'update' in request.POST:
+            task_form = TaskForm(request.POST, instance=task)
+            task_form.save()
+         elif 'delete' in request.POST:
+            task.delete()
+
+         return redirect('home')
