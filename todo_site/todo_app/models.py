a763439c8997b2dcbecb7dda73e4e461ad99c018
sobrien-banyan <sobrien@banyanlabs.io>
2022-12-01 17:54:37 -0700

    task_tags

--- todo_site/todo_app/models.py
+++ todo_site/todo_app/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
 class Task(models.Model):
     description = models.CharField(max_length=255)
+    tags = models.ManyToManyField(Tag)
 
 class Comment(models.Model):
     body = models.TextField()
